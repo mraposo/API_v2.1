aad7ab20077f5090815ab206ec7bef9ad64e83b6
mraposo <mario.raposo@gmail.com>
2022-08-18 17:07:01 +0200

    New Features

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
+import { CreateReportDto } from './dtos/create-report.dto';
+import { ReportsService } from './reports.service';
+import { UseGuards } from '@nestjs/common';
+import { AuthGuard } from '../guards/auth.guard';
+import { CurrentUser } from '../users/decorators/current-user.decorator';
+import { User } from '../users/user.entity';
+import { ReportDto } from './dtos/report.dto';
+import { Serialize } from '../interceptors/serialize.interceptor';
 
 @Controller('reports')
-export class ReportsController {}
+export class ReportsController {
+    constructor(private reportService: ReportsService) {}
+
+    @Post()
+    @UseGuards(AuthGuard)
+    @Serialize(ReportDto)
+    createReport(@Body() body: CreateReportDto, @CurrentUser() user: User) {
+        return this.reportService.create(body, user);
+
+    }
+}
--- test/auth.e2e-spec.ts
+++ test/auth.e2e-spec.ts
@@ -15,18 +15,36 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
-  it('handles a signup request', () => {
-    const email = 'asdf1@asdf.com';
+    it('handles a signup request', () => {
+        const email = 'asdf1@asdf.com';
 
-    return request(app.getHttpServer())
-      .post('/auth/signup')
-      .send({ email: email, password: 'asdf'})
-      .expect(201)
-      .then((res) => {
-        const { id, email } = res.body;
-        expect(id).toBeDefined();
-        expect(email).toEqual(email);
-      });
+        return request(app.getHttpServer())
+        .post('/auth/signup')
+        .send({ email: email, password: 'asdf'})
+        .expect(201)
+        .then((res) => {
+            const { id, email } = res.body;
+            expect(id).toBeDefined();
+            expect(email).toEqual(email);
+        });
 
-  });
+    });
+
+    it('signup as a new user then get the currently logged in user', async () => {
+        const email = 'asdf@asdf.com';
+
+        const res = await request(app.getHttpServer())
+            .post('/auth/signup')
+            .send({ email, password: 'asdf'})
+            .expect(201)
+
+        const cookie = res.get('Set-Cookie');
+
+        const { body } = await request(app.getHttpServer())
+            .get('/auth/whoami')
+            .set('Cookie', cookie)
+            .expect(200)
+            
+        expect(body.email).toEqual(email);
+    });
 });
